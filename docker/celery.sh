8312ee47e3c1b658a73a7fb39217b109806389bf
Zin <svetloewill@mail.ru>
2023-06-20 23:34:21 +0300

    start

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-celery --app=tasks.celery worker --loglevel=INFO --concurrency 4 -P eventlet
+
+#celery --app=tasks.celery worker --loglevel=INFO -P solo
 #celery --app=tasks.celery beat --loglevel=INFO
 #celery --app=tasks.celery flower --loglevel=INFO
+
+worker: python main.py
--- tasks.py
+++ tasks.py
@@ -8,16 +8,15 @@ celery = Celery('tasks', broker=f'redis://{REDISUSER}:{REDIS_PASS}@{REDIS_HOST}:
 
 CELERYD_MAX_TASKS_PER_CHILD = 1  # 单work最多任务使用数
 CELERYD_CONCURRENCY = 3  # 单worker最大并发数
-CELERYD_MAX_MEMORY_PER_CHILD = 500 # 单任务可占用2G内存
+CELERYD_MAX_MEMORY_PER_CHILD = 2000 # 单任务可占用2G内存
 CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24 * 3
 CELERYD_HIJACK_ROOT_LOGGER = False
 
 
 @celery.task
 def send_resume():
-    # res = Update()
-    # result = res.update_resume()
-    result =204
+    res = Update()
+    result = res.update_resume()
     if result == 204:
         return 'Обновлено'
     elif result == 429:
