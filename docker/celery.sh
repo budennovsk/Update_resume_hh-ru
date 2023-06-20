5f795c3ab41d7a3578f82cf6ebc232dff1998f19
Zin <svetloewill@mail.ru>
2023-06-20 22:53:51 +0300

    flower16

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-celery --app=tasks.celery worker --loglevel=INFO --max-tasks-per-child=1
+celery --app=tasks.celery worker --loglevel=INFO
 #celery --app=tasks.celery beat --loglevel=INFO
 celery --app=tasks.celery flower --loglevel=INFO
--- tasks.py
+++ tasks.py
@@ -10,13 +10,14 @@ CELERYD_MAX_TASKS_PER_CHILD = 1  # 单work最多任务使用数
 CELERYD_CONCURRENCY = 3  # 单worker最大并发数
 CELERYD_MAX_MEMORY_PER_CHILD = 1024*1024*2  # 单任务可占用2G内存
 CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24 * 3
-CELERYD_HIJACK_ROOT_LOGGER = False
+# CELERYD_HIJACK_ROOT_LOGGER = False
 
 
 @celery.task
 def send_resume():
-    res = Update()
-    result = res.update_resume()
+    # res = Update()
+    # result = res.update_resume()
+    result = 204
     if result == 204:
         return 'Обновлено'
     elif result == 429:
