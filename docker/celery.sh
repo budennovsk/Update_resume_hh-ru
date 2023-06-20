00445a1acad869efa37206b440042c1864e257f1
Zin <svetloewill@mail.ru>
2023-06-20 18:59:54 +0300

    flower1

--- docker/celery.sh
+++ docker/celery.sh
@@ -3,4 +3,4 @@
 
 celery --app=tasks.celery worker --loglevel=INFO
 celery --app=tasks.celery beat --loglevel=INFO
-celery --app=tasks.celery flower --loglevel=INFO
+#celery --app=tasks.celery flower --loglevel=INFO
