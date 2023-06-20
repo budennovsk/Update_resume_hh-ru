95f679725aa68b7ce532f0a43a83992cad2cc88d
Zin <svetloewill@mail.ru>
2023-06-20 19:06:46 +0300

    flower3

--- docker/celery.sh
+++ docker/celery.sh
@@ -4,11 +4,3 @@
 celery --app=tasks.celery worker --loglevel=INFO
 #celery --app=tasks.celery beat --loglevel=INFO
 #celery --app=tasks.celery flower --loglevel=INFO
-chown -R nobody:nogroup /var/run/celery /var/log/celery
-
-exec celery --app=tasks.celery worker \
-            --loglevel=INFO --logfile=/var/log/celery/worker-example.log \
-            --statedb=/var/run/celery/worker-example@%h.state \
-            --hostname=worker-example@%h \
-            --queues=celery.example -O fair \
-            --uid=nobody --gid=nogroup
