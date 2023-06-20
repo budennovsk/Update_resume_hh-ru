e69712d2b792cbde44e99fa27278e364032fcbe5
Zin <svetloewill@mail.ru>
2023-06-20 23:20:36 +0300

    yes6

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-celery --app=tasks.celery worker --loglevel=INFO
+celery --app=tasks.celery worker --loglevel=INFO --concurrency 1 -P solo
 #celery --app=tasks.celery beat --loglevel=INFO
 #celery --app=tasks.celery flower --loglevel=INFO
